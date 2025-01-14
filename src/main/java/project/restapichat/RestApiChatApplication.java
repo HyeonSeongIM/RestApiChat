d6177c16e272ef5d0e23a1603ad05a492a3b8b2b
HyeonSeong <gus8131@naver.com>
2025-01-14 13:08:09 +0900

    base

---
+++ src/main/java/project/restapichat/RestApiChatApplication.java
@@ -0,0 +1,13 @@
+package project.restapichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/restapichat/RestApiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package project.restapichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
